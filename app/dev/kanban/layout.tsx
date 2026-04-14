aeb226a1c446b41d443b46b0db511768eef32b7f
amineseomaniak <amineseomaniak@gmail.com>
2026-04-14 15:33:11 +0100

    create kanban page update

---
+++ app/dev/kanban/layout.tsx
@@ -0,0 +1,27 @@
+import { AppSidebar } from "@/components/screens/dashboard/app-sidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { ThemeToggle } from "@/components/ui/theme-toggle"
+import { ReactNode } from "react"
+
+export default function DevLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <SidebarProvider defaultOpen={false}>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-12 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <div className="ml-auto">
+            <ThemeToggle />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col p-4 overflow-hidden h-full">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}

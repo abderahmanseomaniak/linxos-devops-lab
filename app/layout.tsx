8ce6689213e11a8693bf1a98dd550cbc1df18d9a
Fahd AHSAYNI <fahdahsayni@gmail.com>
2026-04-22 15:00:25 +0100

    feat: install input-otp package and implement root layout with theme and tooltip providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Skiper89 } from "@/components/ui/skiper-ui/skiper89";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,8 +31,6 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-screen overflow-auto">
-              <Skiper89 />
-
         <ThemeProvider
           attribute="class"
           defaultTheme="system"

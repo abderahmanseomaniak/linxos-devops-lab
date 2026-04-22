e1078dffb369c6931b388a63c7521f2ffb0e4782
Fahd AHSAYNI <fahdahsayni@gmail.com>
2026-04-22 20:33:34 +0100

    feat: initialize root layout with theme provider, tooltip provider, and custom font configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="fr"
-      className={cn("h-full", "antialiased", "bg-background", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("h-full", "antialiased", "bg-background", "font-sans", inter.variable)}
       suppressHydrationWarning
     >
       <body className="min-h-screen overflow-auto">

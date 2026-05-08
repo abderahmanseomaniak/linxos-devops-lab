7af8f9afb0ec34cce11a4a3c497c1650b4b4642a
naoufalseomaniak <naoufalseomaniak@gmail.com>
2026-05-08 14:57:12 +0100

    amélioration des composant

--- components/screens/kanban/kanban-column.tsx
+++ components/screens/kanban/kanban-column.tsx
@@ -33,7 +33,7 @@ export function KanbanColumn({ stage, events }: KanbanColumnProps) {
         </Typography>
       </div>
 
-      <div className="flex flex-col gap-2 overflow-x-hidden overflow-y-auto flex-1 pr-1 min-h-0">
+      <div className="flex flex-col gap-2 overflow-y-auto flex-1 pr-1 min-h-0 max-h-[calc(100vh-220px)]">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
